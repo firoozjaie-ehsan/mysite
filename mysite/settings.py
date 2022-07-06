98172e72f88df52e63bce846ee40d8c26edf7237
ehsan <firoozjaieehsan@gmail.com>
2022-07-06 12:49:35 +0430

    added static directory and url setting
    +static root
    +static url
    +media url
    +media root

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'django-insecure-+po49rto0d_^5t0=6n%%=p_z@95e)7ly+t4cjgdat5vh5j-+vt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = []
 
 
@@ -117,7 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR /'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
