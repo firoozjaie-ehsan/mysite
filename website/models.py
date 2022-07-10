73b1a0b1ae1d65c8201f9dd6cd2d7cc9e232ef59
ehsan <firoozjaieehsan@gmail.com>
2022-07-10 17:59:26 +0430

    create table Post Contact and register they

--- website/models.py
+++ website/models.py
@@ -1,3 +1,17 @@
+import email
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    email=models.EmailField()
+    subject = models.CharField(max_length=255)
+    massage= models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = ('create_date',)
+    def __str__(self):
+        return self.name
