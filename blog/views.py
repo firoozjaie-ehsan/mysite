57a73b8f3ca33290c81bb5d8c1fe47c2a2cd974b
ehsan <firoozjaieehsan@gmail.com>
2022-08-11 19:13:52 +0430

    next and prev post linked

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,get_object_or_404
 import datetime
 from blog.models import Post
 from django.utils import timezone
+from next_prev import next_in_order, prev_in_order
+
 # Create your views here.
 
 def blog_view(request):
@@ -14,10 +16,26 @@ def blog_view(request):
     return render(request,'blog/blog-home.html',context)
 
 def blog_single(request,pid):
+    posts = Post.objects.all().order_by('-create_date')
+    # default ordering
+    first = Post.objects.first()
+    second = next_in_order(first)
+    prev_in_order(second) == first # True
+    last = prev_in_order(first, loop=True)
 
     post=get_object_or_404(Post,pk=pid,status=1)
+    prev=post
+    flag=0
+    #کد من کاری خواسته شده انجام میده جز شرطی که برای اول اخر لیست میشد گذاشت که نزاشتم ولی احتمالا رش دیگه شاید باش مث پیجینیتور اگه هست لطفا در کامنت میشه لینک برام بزارین؟ 
+    for p in posts:
+        if p!=post and flag==0:
+            prev=p
+            flag=1
+        elif flag==1:
+            next=p
+            break
     post.incrementViewCount()
-    context={'post':post}
+    context={'post':post,'posts':posts,'prev':prev,'next':next}
     return render(request,'blog/blog-single.html',context)
 
 
