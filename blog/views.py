c252ba5daea1f594c019c92e8d3d20c08d3b960c
ehsan <firoozjaieehsan@gmail.com>
2022-08-14 11:41:31 +0430

    prev and next post section edited

--- blog/views.py
+++ blog/views.py
@@ -10,22 +10,22 @@ def blog_view(request):
     # end_date = timezone.now().strftime("%Y-%m-%d %H:%M:%S")
     # posts=Post.objects.filter(published_date__range=(Post.published_date.strftime("%Y-%m-%d %H:%M:%S"), end_date))
     
-    #منظور اگه پستهایی که تا الان منتظر شدن نمایش بده هست فک کنم جواب اینه اگه نده نتونستم جواب پیدا کنم
-    posts=Post.objects.filter(published_date__lte=timezone.now())
+    posts=Post.objects.filter(published_date__lte=timezone.now(),status=1)
     context={'posts':posts}
     return render(request,'blog/blog-home.html',context)
 
 def blog_single(request,pid):
-    posts = Post.objects.all().order_by('-create_date')
+    posts = Post.objects.filter(status=1).order_by('-create_date')
     # default ordering
 
     post=get_object_or_404(Post,pk=pid,status=1)
-    prev=post
+    prev=None
+    next=None
     flag=0
-    #کد من کاری خواسته شده انجام میده جز شرطی که برای اول اخر لیست میشد گذاشت که نزاشتم ولی احتمالا رش دیگه شاید باش مث پیجینیتور اگه هست لطفا در کامنت میشه لینک برام بزارین؟ 
     for p in posts:
         if p!=post and flag==0:
             prev=p
+        elif p==post:
             flag=1
         elif flag==1:
             next=p
